936dd353389a07bca4d6828995cee36e27c44787
JacobSingh <jacobsingh@gmail.com>
2021-11-09 12:58:39 +0530

    added an option and some verbosity

--- get_stats.sh
+++ get_stats.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 function get_repos() { 
-    repo=$1
-    curl -u "$GH_USER:$GH_TOKEN" -H "Accept: application/vnd.github.v3+json"  "https://api.github.com/orgs/$repo/repos?type=private&per_page=100" | jq '.[].name' | sed -e 's:"::g'
+    org=$1
+    type="private"
+    if [ -n $GH_STATS_REPO_TYPE ]; then
+        type=$GH_STATS_REPO_TYPE
+    fi
+    curl -u "$GH_USER:$GH_TOKEN" -H "Accept: application/vnd.github.v3+json"  "https://api.github.com/orgs/$org/repos?type=$type&per_page=100" | jq '.[].name' | sed -e 's:"::g'
     echo 
 }
 
-function usage() { echo "Usage: $0 org [-o stats_directory];" 1>&2; exit 1; }
+function usage() { echo "Usage: $0 [-o stats_directory] org;
+For public repos, set env variable GH_STATS_REPO_TYPE to public" 1>&2; exit 1; }
 
 OUTPUT="."
 
@@ -42,5 +47,9 @@ set +x
 echo "Getting stats for $ORG and putting in $OUTPUT"
 for REPO in $(get_repos $ORG); do
     echo $REPO
+    # pr-stats isn't mobile, so moving over there 
+    cd $(dirname `which pr-stats`)
+    pwd
     pr-stats --oauth $GH_TOKEN $ORG $REPO > $OUTPUT/$REPO.txt
+    cd -
 done;
